2a046d50ac402c2cb909747dd8e916acdee17be2
Alejandro Campos <acamposcar@gmail.com>
2022-04-04 03:52:03 +0200

    Remove / from files url

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ const weatherObj = (data, isCurrent) => {
   const summary = data.weather[0].main;
   const description = data.weather[0].description;
   const icon = data.weather[0].icon;
-  const iconURL = `/svg/${icon}.svg`;
+  const iconURL = `svg/${icon}.svg`;
 
   const pressure = data.pressure;
   const humidity = data.humidity;
